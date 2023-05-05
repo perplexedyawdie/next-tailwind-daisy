8421ea6d8aadd22f66194039a12e54466ce14029
Javel Rowe <github@northway.pro>
2023-05-04 23:24:45 -0500

    added mongo boilerplate + removed unused deps

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,14 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
-import DiscordProvider from "next-auth/providers/discord";
-
-
 
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!
-    })
   ],
+  adapter: MongoDBAdapter(dbObj.clientProm),
+  session: {
+    strategy: "jwt"
+  }
 }
 
 export default NextAuth(authOptions)
